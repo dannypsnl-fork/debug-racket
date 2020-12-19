c5a97580bf0b9d9f1b579306af97182cf9d466f3
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2020-12-19 15:28:30 +0800

    prompt

--- main.rkt
+++ main.rkt
@@ -3,6 +3,13 @@
 (require (for-syntax syntax/parse))
 (require "env.rkt")
 
+(define (prompt)
+  (write "> ")
+  (define e (read))
+  (match e
+    ['env (displayln (cur-env))]
+    [else (printf "unknown command: `~a`~n" e)]))
+
 (define-syntax (debug-define stx)
   (syntax-parse stx
     #:literals (debug-define)
@@ -10,14 +17,15 @@
      #'(begin
          (define name exp)
          (env/bind (syntax-e #'name) exp))]
-    [(debug-define (name:id param*:id ...) body)
+    [(debug-define (name:id param*:id ...) e* ... e)
      #'(define (name param* ...)
          (parameterize ([cur-env (make-env)])
            (for ([p-name (list #'param* ...)]
                  [p (list param* ...)])
              (env/bind (syntax-e p-name) p))
-           (displayln (cur-env))
-           body))]
+           (prompt)
+           e* ...
+           e))]
     ;; since rest form should be invalid, we rebuild define form to get error
     [(debug-define . any) #'(define . any)]))
 
