7fbabea7464ec163c9570719430c2e5b537cd7fe
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2020-12-22 07:49:41 +0800

    debug-let and continute command

--- main.rkt
+++ main.rkt
@@ -7,8 +7,11 @@
   (display "> ")
   (define e (read))
   (match e
-    ['env (displayln (cur-env))]
-    [else (printf "unknown command: `~a`~n" e)]))
+    ['env (displayln (cur-env))
+          (prompt)]
+    ['c (void)]
+    [else (printf "unknown command: `~a`~n" e)
+          (prompt)]))
 
 (define-syntax (debug-define stx)
   (syntax-parse stx
@@ -24,12 +27,28 @@
                  [p (list param* ...)])
              (env/bind (syntax-e p-name) p))
            (~@ (prompt) e*) ...
-           (prompt)
-           e))]
+           (prompt) e))]
     ;; since rest form should be invalid, we rebuild define form to get error
     [(debug-define . any) #'(define . any)]))
+
+(define-syntax (debug-let stx)
+  (syntax-parse stx
+    #:literals (debug-let)
+    [(debug-let ([binding-name*:id binding-expr*] ...) e* ... e)
+     #'(let ([binding-name* binding-expr*] ...)
+         (parameterize ([cur-env (make-env)])
+           (for ([b-name (list #'binding-name* ...)]
+                 [b-e (list binding-expr* ...)])
+             (env/bind (syntax-e b-name) b-e))
+           (~@ (prompt) e*) ...
+           (prompt) e))]
+    [(debug-let . any) #'(let . any)]))
 
 (debug-define foo 1)
 (debug-define (id x) x)
 
 (id 2)
+
+(debug-let ([a 1] [b 2])
+  (displayln b)
+  a)
